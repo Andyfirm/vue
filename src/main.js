02f22a43e5dfc291ae21bb1076612bc5983bb4da
maxiaofeng <2370898175@qq.com>
2018-08-13 20:18:58 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import './assets/fonts/iconfont.css'
+import './assets/css/global.css'
+import Axios from 'axios'
+
+Vue.use(ElementUI)
+
+Axios.defaults.baseURL = 'https://www.escook.cn:8888/api/private/v1/'
+Vue.prototype.$http = Axios
 
 Vue.config.productionTip = false
 
